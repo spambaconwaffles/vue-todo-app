f423919b3a0a7f8fcfd13dac0dc05f614dcc4c15
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-08-03 16:48:03 +0800

    work on click outside to change back to normal paragraph, update

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -1,10 +1,10 @@
 <template>
     <li class="flex mb-4 items-center">
-        <p class="w-full text-grey-darkest">{{ snippet }}</p>
+        <p v-show="!showInputBox" class="w-full text-grey-darkest" @dblclick="updateTodo">{{ todo_item.todo_desc }}</p>
+        <input v-show="showInputBox" class="w-full" type="text" v-model="updatedItem">
         <button
             class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-500">Done</button>
-        <button
-            class="flex-no-shrink p-2 ml-2 border-2 rounded text-yellow-500 border-yellow-500 hover:text-white hover:bg-yellow-500">Update</button>
+        
         <button @click="removeTodo"
             class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Remove</button>
     </li>
@@ -13,21 +13,9 @@
 
 <script setup>
 
-import { ref, computed, defineProps, inject } from 'vue';
+import { ref, defineProps, inject } from 'vue';
 
 const props = defineProps(["todo_item"])
-
-const snippet = computed(() => {
-    let itemDesc = props.todo_item.todo_desc
-
-    let cutoffLength = 50
-
-    if (itemDesc.length > cutoffLength) {
-        return itemDesc.substring(0, cutoffLength) + "..."
-    }
-
-    return itemDesc
-})
 
 
 const error = inject("error")
@@ -63,6 +51,13 @@ const removeTodo = async () => {
 }
 
 
+// update specific item by id
+const updatedItem = ref(props.todo_item.todo_desc)
+const showInputBox = ref(false)
+
+const updateTodo = () => {
+    showInputBox.value = !showInputBox.value
+}
 
 
 </script>
