0677a59f619b6b11434f1c633c3b68868ac5ea0e
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-08-03 21:37:00 +0800

    added dynamic routing for update, now need to do update request

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -1,10 +1,10 @@
 <template>
     <li class="flex mb-4 items-center">
-        <p v-show="!showInputBox" class="w-full text-grey-darkest" @dblclick="updateTodo">{{ todo_item.todo_desc }}</p>
-        <input v-show="showInputBox" class="w-full" type="text" v-model="updatedItem">
-        <button
-            class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-500">Done</button>
-        
+        <p class="w-full text-grey-darkest">{{ snippet }}</p>
+        <!-- <button
+            class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-500">Done</button> -->
+        <router-link :to="`/item/${todo_item.id}`"
+            class="flex-no-shrink p-2 ml-2 border-2 rounded text-yellow-500 border-yellow-500 hover:text-white hover:bg-yellow-500">Update</router-link>
         <button @click="removeTodo"
             class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Remove</button>
     </li>
@@ -13,12 +13,25 @@
 
 <script setup>
 
-import { ref, defineProps, inject } from 'vue';
+import { ref, computed, defineProps, inject } from 'vue';
 
 const props = defineProps(["todo_item"])
 
 
 const error = inject("error")
+
+// If length of todo item is over a specified number,
+// only the first few characters will be shown
+let cutoffLength = 3
+const snippet = computed(() => {
+    let itemDesc = props.todo_item.todo_desc
+    if (itemDesc.length > cutoffLength) {
+        return itemDesc.substring(0, cutoffLength) + "..."
+    }
+    return itemDesc
+})
+
+
 
 // Inject todo_list ref from parent component to access
 // for delete and update
@@ -29,7 +42,7 @@ const removeTodo = async () => {
     try {
         const deleteId = props.todo_item.id
         console.log("Deleting ", deleteId)
-        
+
         // remove item from todo_list ref
         todo_list.value = todo_list.value.filter((item) => {
             return item.id !== deleteId
@@ -48,15 +61,6 @@ const removeTodo = async () => {
         error.value = err.message
         console.log(error.value)
     }
-}
-
-
-// update specific item by id
-const updatedItem = ref(props.todo_item.todo_desc)
-const showInputBox = ref(false)
-
-const updateTodo = () => {
-    showInputBox.value = !showInputBox.value
 }
 
 
