93d0f5ffdb82e7ce2dd28574c0e9fe2db6b8b9c2
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-08-04 14:32:35 +0800

    styling done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto px-4">
+    <h1 class="text-2xl font-medium mb-4">About</h1>
+    <p>Hello welcome to my Todo web app</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi vel voluptatum recusandae quos iusto ducimus
+      saepe fuga ab dolorum facilis esse dolores cumque, qui delectus voluptas similique minus quia.</p>
+
+    <div class="h-screen grid content-end justify-items-center	">
+      <button @click="toggleMiku">Miku</button>
+      <img v-show="showMiku" src="https://media.tenor.com/4mGl-qVGALAAAAAM/miku-anime.gif" alt="miku">
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showMiku = ref(false)
+
+const toggleMiku = () => {
+  showMiku.value = !showMiku.value
+}
+
+</script>
