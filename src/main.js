990062b5ac1283c9fe172fe9e31353033e3d2773
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-08-05 12:13:26 +0800

    setup toast

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+import Toast from 'vue-toastification'
+import "vue-toastification/dist/index.css"
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).use(Toast).mount('#app')
